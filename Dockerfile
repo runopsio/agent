runopsio/agent

commit 4ae424c7b27fd6f3bf4fd85453ff1d4c29dc95e8

Add python3 missing libs

Dockerfile
@@ -13,11 +13,8 @@ RUN apt-get update -y && \
         postgresql-client-13
 
 RUN pip3 install -U \
-    boto3==1.18.31 \
-    pytz==2021.1 \
-    pandas==1.3.2 \
-    SQLAlchemy==1.4.23 \
     appdirs==1.4.4 \
+    boto3==1.18.31 \
     CacheControl==0.12.6 \
     chardet==4.0.0 \
     colorama==0.4.4 \
@@ -29,11 +26,16 @@ RUN pip3 install -U \
     msgpack==1.0.2 \
     ordered-set==4.0.2 \
     packaging==20.9 \
+    pandas==1.3.2 \
     pep517==0.9.1 \
     progress==1.5 \
+    psycopg2-binary==2.9.1 \
     pyparsing==2.4.7 \
     pytoml==0.1.21 \
+    pytz==2021.1 \
     retrying==1.3.3 \
+    requests==2.26.0 \
+    SQLAlchemy==1.4.23 \
     toml==0.10.2 \
     webencodings==0.5.1
 

commit dca4d282d1b29e86d17c06801843470771811b40

Add missing python3 libs, due to regression in commit 4ae424c7

Dockerfile
@@ -16,9 +16,6 @@ RUN apt-get update -y && \
 
 RUN pip3 install -U \
     boto3==1.18.31 \
-    pytz==2021.1 \
-    pandas==1.3.2 \
-    SQLAlchemy==1.4.23 \
     appdirs==1.4.4 \
     CacheControl==0.12.6 \
     chardet==4.0.0 \
@@ -30,12 +27,17 @@ RUN pip3 install -U \
     lockfile==0.12.2 \
     msgpack==1.0.2 \
     ordered-set==4.0.2 \
+    pandas==1.3.2 \
     paramiko==2.7.2 \
     packaging==20.9 \
     pep517==0.9.1 \
     progress==1.5 \
+    psycopg2-binary==2.9.1 \
     pyparsing==2.4.7 \
     pytoml==0.1.21 \
+    pytz==2021.1 \
+    requests==2.26.0 \
+    SQLAlchemy==1.4.23 \
     retrying==1.3.3 \
     toml==0.10.2 \
     webencodings==0.5.1