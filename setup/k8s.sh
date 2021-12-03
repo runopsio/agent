31442a4c10ffa65a18610c2645f45104c8a96678
Sandro Mello <sandromll@gmail.com>
2021-12-03 09:38:37 -0300

    Add agent setup (#41)
    * Add agent setup

---
+++ setup/k8s.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+set -eo pipefail
+
+AGENT_TOKEN=${AGENT_TOKEN:?Is required to be exported}
+
+kubectl create namespace runops || true
+kubectl -n runops delete secret agent-config || true
+kubectl -n runops create secret generic agent-config \
+  --from-literal=TOKEN=$AGENT_TOKEN \
+  --from-literal=TAGS=$AGENT_TAG \
+  --from-literar=JWK_URL=$AGENT_JWK_URL \
+  --from-literal=ENV_CONFIG=$ENV_CONFIG \
+  --from-literal=AWS_SECRET_MANAGER=false
+kubectl delete po -n runops -l app=agent || true
+
+kubectl -n runops apply -n runops -f - <<EOF
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  labels:
+    app: agent
+  name: agent
+spec:
+  replicas: 1
+  selector:
+    matchLabels:
+      app: agent
+  template:
+    metadata:
+      labels:
+        app: agent
+    spec:
+      containers:
+      - envFrom:
+        - secretRef:
+            name: agent-config
+        image: runops/agent
+        imagePullPolicy: Always
+        name: agent
+EOF
+
+echo ""
+kubectl -n runops get pod
+echo "------"
+kubectl -n runops get secrets
