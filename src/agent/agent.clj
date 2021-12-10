bf19f764571c338936d261e0d11726127e902c8d
Sandro Mello <sandromll@gmail.com>
2021-12-10 14:33:05 -0300

    Increase webhook timeout conn (#46)
    * Increase webhook timeout conn
    * Increase times to attempt to send webhook calls

--- src/agent/agent.clj
+++ src/agent/agent.clj
@@ -271,7 +271,7 @@ fi")
 (defn webhook-grpc-call [client data]
   (try
     [(locking webhook-call-lock
-       (deref (grpc-client/Webhook client data) (backoff/sec->ms 5) nil)) nil]
+       (deref (grpc-client/Webhook client data) (backoff/sec->ms 15) nil)) nil]
     (catch Exception e
       ;; try to reconnect on errors and wait a few seconds
       ;; always lock to prevent other threads from reusing it
@@ -283,10 +283,9 @@ fi")
 
 (defmulti webhook (fn [task] (:mode task)))
 (defmethod webhook :grpc [task]
-  (log/info {:client-alive (clients/grpc-client-alive?)}
-            (format "Starting task webhook via gRPC for task id [%s] with status [%s]" (:id task) (:status task)))
+  (log/info (format "Starting task webhook via gRPC for task id [%s] with status [%s]" (:id task) (:status task)))
   (try
-    (let [[res err] (loop [attempts 3 grpc-response nil err nil]
+    (let [[res err] (loop [attempts 5 grpc-response nil err nil]
                       (if (or (not (empty? grpc-response)) (= attempts 0)) [grpc-response err]
                           (let [[res err] (webhook-grpc-call (clients/grpc-client) (dissoc task :mode))]
                             (recur (dec attempts) res err))))
