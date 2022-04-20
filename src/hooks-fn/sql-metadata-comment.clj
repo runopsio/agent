a9e61e77fdb27708541cedfa644e7da1b9771237
Sandro Mello <sandromll@gmail.com>
2022-04-20 11:29:08 -0300

    Add pre-hook function to add a comment in sql queries in a best effort manner

--- src/hooks-fn/sql-metadata-comment.clj
+++ src/hooks-fn/sql-metadata-comment.clj
@@ -4,7 +4,17 @@
   :function
   (fn [task-context]
     (let [script (:script task-context)
-          user-email (:user-email task-context)
-          task-id (:task-id task-context)]
-      (str (format "/** Runops task-id %s, started by user %s **/ " task-id user-email)
-           script)))}}
+          sql-comment (format "/* Runops task-id %s, started by user %s */ "
+                              (:task-id task-context) (:user-email task-context))
+          query-list (clojure.string/split-lines script)
+          query-list (->> query-list
+                          (map-indexed
+                           (fn [idx q]
+                             (let [q (clojure.string/trim q)]
+                               (if (clojure.string/ends-with? q ";")
+                                 (str sql-comment q)
+                                 ;; always add it to the first line
+                                 (if (= idx 0)
+                                   (str sql-comment q)
+                                   q))))))]
+      (clojure.string/join "\n" query-list)))}}
