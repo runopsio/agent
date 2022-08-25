f8a69b145d8c416f175046efc708c07725a1fc89
Sandro Mello <sandromll@gmail.com>
2022-08-25 15:47:22 -0300

    Change port

--- Dockerfile
+++ Dockerfile
@@ -186,7 +186,7 @@ ADD target/uberjar/agent-$VERSION-standalone.jar /agent/app.jar
 COPY rootfs/ /
 
 # webapp ui
-EXPOSE 8280
+EXPOSE 8000
 
 ENTRYPOINT ["tini", "--"]
 CMD ["/usr/local/bin/run-agent.sh"]
--- rootfs/usr/local/bin/run-agent.sh
+++ rootfs/usr/local/bin/run-agent.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-nohup /usr/local/lib/node_modules/serve/build/main.js --listen tcp://0.0.0.0:8280 -s /agent/ui & 
+nohup /usr/local/lib/node_modules/serve/build/main.js --listen tcp://0.0.0.0:8000 -s /agent/ui & 
 java -jar /agent/app.jar
