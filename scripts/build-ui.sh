2dbdfbf32d0606e7a7057cf387519a646da41c01
Sandro Mello <sandromll@gmail.com>
2022-08-25 14:36:59 -0300

    fix build ui script

--- scripts/build-ui.sh
+++ scripts/build-ui.sh
@@ -4,5 +4,5 @@ mkdir -p build && rm -rf ./build/webapp
 git clone git@github.com:runopsio/webapp.git ./build/webapp && cd ./build/webapp
 npm install
 npm run release:agent-ui
-mv resources/public ../../rootfs/agent/ui
+cp -a resources/public/* ../../rootfs/agent/ui/
 cd -
