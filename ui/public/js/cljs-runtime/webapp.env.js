8ad88ec885f389651471ee617d2d7083251a3444
Rogerio Moura <roger0.rm@gmail.com>
2022-08-25 18:45:17 -0300

    fix(ui): remove ui folder

--- ui/public/js/cljs-runtime/webapp.env.js
+++
@@ -1,7 +0,0 @@
-goog.provide('webapp.env');
-/**
- * @define {string}
- */
-webapp.env.release_type = goog.define("webapp.env.release_type","");
-
-//# sourceMappingURL=webapp.env.js.map
--- ui/public/js/compiled/cljs-runtime/shadow.module.app.append.js
+++
@@ -1,4 +0,0 @@
-
-shadow.cljs.devtools.client.env.module_loaded('app');
-
-try { webapp.core.init(); } catch (e) { console.error("An error occurred when calling (webapp.core/init)"); throw(e); }
