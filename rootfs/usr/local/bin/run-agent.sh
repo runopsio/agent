abcef64f235d6322ff74f3384011144afcfc25e2
Sandro Mello <sandromll@gmail.com>
2022-10-25 10:44:29 -0300

    Adds option to disable gosu with the env GOSU_DISABLE=true (#111)

--- rootfs/usr/local/bin/run-agent.sh
+++ rootfs/usr/local/bin/run-agent.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 nohup /usr/local/lib/node_modules/serve/build/main.js --listen tcp://0.0.0.0:8000 -s /agent/ui & 
+if [[ "${GOSU_DISABLE}" == "true" ]]; then
+    # it will override /usr/sbin/gosu
+    mv /usr/local/bin/fake-gosu.sh /usr/local/bin/gosu
+fi
 java -jar /agent/app.jar
